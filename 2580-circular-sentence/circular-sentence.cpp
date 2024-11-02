f8b8bb48cc96e04ad1d842130e524ff9b9d17085
Alok Kumar <137555189+Alokkr41134@users.noreply.github.com>
2024-11-02 20:40:33 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (12.39%) - LeetSync

---
+++ 2580-circular-sentence/circular-sentence.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isCircularSentence(string sentence) {
+           istringstream iss(sentence);
+        vector<string> words;
+        string word;
+
+        while (iss >> word) {
+            words.push_back(word);
+        }
+
+        // Check if the last character of each word matches the first character of the next word
+        int n = words.size();
+        for (int i = 0; i < n; ++i) {
+            // Last character of the current word
+            char lastChar = words[i].back();
+            // First character of the next word (circularly)
+            char firstChar = words[(i + 1) % n][0];
+
+            if (lastChar != firstChar) {
+                return false;
+            }
+        }
+
+        return true; 
+    }
+};
