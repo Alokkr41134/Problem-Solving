ceab0903537c5cf6a96827a23a508c0152b92d1f
Alok Kumar <137555189+Alokkr41134@users.noreply.github.com>
2024-11-06 10:55:45 +0530

    Time: 3 ms (49.64%) | Memory: 31.1 MB (86.44%) - LeetSync

--- 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.cpp
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.cpp
@@ -2,16 +2,22 @@ class Solution {
 public:
     bool canSortArray(vector<int>& nums) {
         int n=nums.size();
+        bool sorted=true;
         for(int i=0;i<n;i++){
+            sorted=false;
             for(int j=0;j<n-1-i;j++){
                 if(nums[j]>nums[j+1]){
                     if(__builtin_popcount(nums[j]) == __builtin_popcount(nums[j+1])){
                         swap(nums[j],nums[j+1]);
+                        sorted=true;
                     }
                     else{
                         return false;
                     }
                 }
+            }
+            if(sorted==false){
+                break;
             }
         }
         return true;
